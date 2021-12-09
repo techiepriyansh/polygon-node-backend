b26e9c846028433e8daf38d8f3caa5f1b8141bef
Priyansh Rathi <techiepriyansh@gmail.com>
2021-12-09 22:11:33 +0530

    raw proto done

--- game.js
+++ game.js
@@ -7,6 +7,9 @@ const BLACK = 1;
 const PLAYER_ONE_CONNECTED                = 0;
 const PLAYER_TWO_CONNECTED                = 1;
 const GAME_READY                          = 2;
+const WHITE_TO_MOVE                       = 3;
+const BLACK_TO_MOVE                       = 4;
+const GAME_OVER                           = 5;
 
 
 class Game 
@@ -28,12 +31,44 @@ class Game
 	assignColors()
 	{
 		assert.equal(this.status, PLAYER_TWO_CONNECTED);
+
 		let decider = Math.random() > 0.5;
-		player1.color = decider ? WHITE : BLACK;
-		player2.color = decider ? BLACK : WHITE;
+		this.player1.color = decider ? WHITE : BLACK;
+		this.player2.color = decider ? BLACK : WHITE;
+
+		this.white = decider ? this.player1 : this.player2; 
+		this.black = decider ? this.player2 : this.player1; 
+
 		this.status = GAME_READY;
 	}
 
+	startGame()
+	{
+		assert.equal(this.status, GAME_READY);
+		this.status = WHITE_TO_MOVE;
+		this.playerToMove = this.white;
+	}
+
+	makeMove(player, _from, _to)
+	{
+		assert.equal(player, this.playerToMove);
+		// let valid = chessEngine.isValid(_from, _to);
+		let valid = true; 
+		if (!valid) return [false, false];
+
+		// let won = chessEngine.didWin(_from, _to);
+		let won = false;
+		if (won) {
+			this.status = GAME_OVER;
+			return [valid, won]
+		}
+
+		let decider = this.status = WHITE;
+		this.playerToMove = decider ? this.white : this.black; 
+		this.status = decider ? BLACK : WHITE;
+
+		return [valid, won];
+	}
 
 }
 
--- index.js
+++ index.js
@@ -20,22 +20,33 @@ wss.on('connection', client => {
 
 		switch (cmd) {
 			case "new_game": {
-				let [pubkey, gameCode] = splitMessage(msg);
+				let [pubkey, gameCode] = splitMessage(arg);
 				let game = new Game(client, pubkey, gameCode);
 				games[gameCode] = game;
 				break;
 			}
 
 			case "join_game": {
-				let [pubkey, gameCode] = splitMessage(msg);
+				let [pubkey, gameCode] = splitMessage(arg);
 				assert.equal(games.hasOwnProperty(gameCode), true);
 				
-				let game = games[gameCode]
+				let game = games[gameCode];
 				game.connectPlayer2(client, pubkey);
 
 				game.assignColors();
 				game.player1.socket.send(`color${MSG_DELIM}${game.player1.color}`);
 				game.player2.socket.send(`color${MSG_DELIM}${game.player2.color}`);
+				game.startGame();
+			}
+
+			case "move": {
+				let [gameCode, move] = splitMessage(arg);
+				assert.equal(games.hasOwnProperty(gameCode), true);
+
+				let game = games[gameCode];
+				let [_from, _to] = splitMessage(move); 
+				let [valid, won] = game.makeMove(client, _from, _to);
+				client.send(`move_valid_won${MSG_DELIM}${valid}${MSG_DELIM}${won}`);
 			}
 		}
 	});
