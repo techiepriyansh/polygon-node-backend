9d6a1cfa7689a0578538ac2151da6d2065ea357e
Angad Kambli <angadkambli@gmail.com>
2021-12-13 23:30:09 +0530

    add streaming

--- index.js
+++ index.js
@@ -14,86 +14,100 @@ var chessContract = new web3.eth.Contract(Chess.abi, contractAddress);
 const { splitMessage, MSG_DELIM } = require("./utils.js");
 const { Game } = require("./game");
 const wss = new WebSocketServer({
-  port: process.env.SOCKET_PORT,
+	port: process.env.SOCKET_PORT,
 });
 
 let games = {};
 
 wss.on("connection", (client) => {
-  client.on("message", (rawMsg) => {
-    let msg = rawMsg.toString();
-    console.log(msg);
-    let [cmd, arg] = splitMessage(msg);
+	client.on("message", (rawMsg) => {
+		let msg = rawMsg.toString();
+		console.log(msg);
+		let [cmd, arg] = splitMessage(msg);
 
-    switch (cmd) {
-      case "new_game": {
-        let [pubkey, gameCode] = splitMessage(arg);
-        let game = new Game(client, pubkey, gameCode);
-        games[gameCode] = game;
-        break;
-      }
+		switch (cmd) {
+			case "new_game": {
+				let [pubkey, gameCode] = splitMessage(arg);
+				let game = new Game(client, pubkey, gameCode);
+				games[gameCode] = game;
+				break;
+			}
 
-      case "join_game": {
-        let [pubkey, gameCode] = splitMessage(arg);
-        // assert.equal(games.hasOwnProperty(gameCode), true);
+			case "join_game": {
+				let [pubkey, gameCode] = splitMessage(arg);
+				// assert.equal(games.hasOwnProperty(gameCode), true);
 
-        let game = games[gameCode];
-        game.connectPlayer2(client, pubkey);
+				let game = games[gameCode];
+				game.connectPlayer2(client, pubkey);
 
-        game.assignColors();
-        game.player1.socket.send(`color${MSG_DELIM}${game.player1.color}`);
-        game.player2.socket.send(`color${MSG_DELIM}${game.player2.color}`);
-        game.startGame();
-        break;
-      }
+				game.assignColors();
+				game.player1.socket.send(
+					`color${MSG_DELIM}${game.player1.color}`
+				);
+				game.player2.socket.send(
+					`color${MSG_DELIM}${game.player2.color}`
+				);
+				game.startGame();
+				break;
+			}
 
-      case "move": {
-        let [gameCode, move] = splitMessage(arg);
-        assert.equal(games.hasOwnProperty(gameCode), true);
+			case "move": {
+				let [gameCode, move] = splitMessage(arg);
+				assert.equal(games.hasOwnProperty(gameCode), true);
 
-        let game = games[gameCode];
-        let [_from, _to] = splitMessage(move);
-        let [valid, isFinished, checkMate] = game.makeMove(client, _from, _to);
+				let game = games[gameCode];
+				let [_from, _to] = splitMessage(move);
+				let [valid, isFinished, checkMate] = game.makeMove(
+					client,
+					_from,
+					_to
+				);
 
-        if (isFinished && checkMate) {
-          var result;
-          let winnerPubkey =
-            client == game.white.socket ? game.white.pubkey : game.black.pubkey; // <- this player won
-          (async () => {
-            result = await chessContract.methods
-              .declareWinner(parseInt(gameCode), winnerPubkey)
-              .send({ from: walletAddress });
-          })();
-          console.log(result);
-        } else if (isFinished && !checkMate) {
-          // draw
-        }
+				if (isFinished && checkMate) {
+					var result;
+					let winnerPubkey =
+						client == game.white.socket
+							? game.white.pubkey
+							: game.black.pubkey; // <- this player won
+					(async () => {
+						result = await chessContract.methods
+							.declareWinner(parseInt(gameCode), winnerPubkey)
+							.send({ from: walletAddress });
+					})();
+					console.log(result);
+				} else if (isFinished && !checkMate) {
+					// draw
+				}
+				let g = games[arg];
+				let currentFEN = game.chessGame.exportFEN();
+				if (valid) {
+					//let opponent = client == game.player1 ? game.player2 : game.player1;
+					console.log(
+						`opponent_move${MSG_DELIM}${_from}${MSG_DELIM}${_to}`
+					);
+					game.player1.socket.send(
+						`opponent_move${MSG_DELIM}${_from}${MSG_DELIM}${_to}`
+					);
+					game.player2.socket.send(
+						`opponent_move${MSG_DELIM}${_from}${MSG_DELIM}${_to}`
+					);
+					wss.clients.forEach(function each(c) {
+						c.send(
+							`stream${MSG_DELIM}${gameCode}${MSG_DELIM}${currentFEN}`
+						);
+					});
+				}
+				break;
+			}
 
-        if (valid) {
-          //let opponent = client == game.player1 ? game.player2 : game.player1;
-          console.log(`opponent_move${MSG_DELIM}${_from}${MSG_DELIM}${_to}`);
-          game.player1.socket.send(
-            `opponent_move${MSG_DELIM}${_from}${MSG_DELIM}${_to}`
-          );
-          game.player2.socket.send(
-            `opponent_move${MSG_DELIM}${_from}${MSG_DELIM}${_to}`
-          );
-          wss.clients.forEach(function each(c) {
-              c.send(
-                `stream${MSG_DELIM}${gameCode}${MSG_DELIM}${_from}${MSG_DELIM}${_to}`
-              );
-            
-          });
-        }
-        break;
-      }
-
-      case "get_game": {
-        let game = games[arg];
-        let currentFEN = game.chessGame.exportFEN();
-        client.send(`init_game${MSG_DELIM}${arg}${MSG_DELIM}${currentFEN}`);
-        break;
-      }
-    }
-  });
+			case "get_game": {
+				let game = games[arg];
+				let currentFEN = game.chessGame.exportFEN();
+				client.send(
+					`init_game${MSG_DELIM}${arg}${MSG_DELIM}${currentFEN}`
+				);
+				break;
+			}
+		}
+	});
 });
